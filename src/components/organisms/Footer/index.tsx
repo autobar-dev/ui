ec78d58eea0559e4ae535e9b6658eb5734798005
jzkulik <j.z.kulik@student.tudelft.nl>
2022-09-18 14:57:33 +0800

    Added global footer

--- src/components/organisms/Footer/index.tsx
+++ src/components/organisms/Footer/index.tsx
@@ -1,12 +1,56 @@
-import { Title } from "@mantine/core";
+import { Container, Group, Button, Text, Center } from "@mantine/core";
+import { IconBrandFacebook, IconBrandYoutube, IconBrandInstagram } from "@tabler/icons";
 import { useStyles } from "./styles";
+import Link from "next/link";
+import Logo from "../../molecules/Logo";
 
 export default function Footer() {
-  const { classes } = useStyles();
+  const { classes, theme } = useStyles();
 
   return (
-    <footer className={classes.root}>
-      <Title>Footer</Title>
-    </footer>
+    <div className={classes.footer}>
+      <Container className={classes.inner}>
+        <Group spacing={8}>
+          <Text color={theme.colors.defaultGray[0]}>
+            &copy; 2022
+          </Text>
+          <Link href="/" passHref>
+            <a style={{
+              display: "flex",
+              alignItems: "center",
+            }}>
+              <Logo
+                textColor={theme.colors.defaultGray[0]}
+                type={"text-only"}
+                size={12}
+              />
+            </a>
+          </Link>
+        </Group>
+        <Group spacing={0} className={classes.links} position="right" noWrap>
+          <Link href="https://www.instagram.com" passHref>
+            <Button component="a" className={classes.icon}>
+              <Center>
+                <IconBrandInstagram size={20} stroke={1.5} color={theme.colors.defaultGray[0]}/>
+              </Center>
+            </Button>
+          </Link>
+          <Link href="https://www.facebook.com" passHref>
+            <Button component="a" className={classes.icon}>
+              <Center>
+                <IconBrandFacebook size={20} stroke={1.5} color={theme.colors.defaultGray[0]}/>
+              </Center>
+            </Button>
+          </Link>
+          <Link href="https://www.youtube.com" passHref>
+            <Button component="a" className={classes.icon}>
+              <Center>
+                <IconBrandYoutube size={20} stroke={1.5} color={theme.colors.defaultGray[0]}/>
+              </Center>
+            </Button>
+          </Link>
+        </Group>
+      </Container >
+    </div >
   );
 }
--- src/components/organisms/Shell/index.tsx
+++ src/components/organisms/Shell/index.tsx
@@ -1,5 +1,3 @@
-import { IconBrandFacebook, IconBrandInstagram } from '@tabler/icons';
-import Link from 'next/link'
 import Footer from '../Footer';
 import Header from '../Header';
 import { useStyles } from './styles';
@@ -17,22 +15,3 @@ export default function Layout({ children }: { children: any }) {
     </div>
   );
 }
-
-/* 
-      <div className={styles.wrapper}>
-        {children}
-      </div>
-
-      <div className={styles.footer}>
-        <span className={styles.footerText}>
-          © 2022 autobar
-        </span>
-        <div className={styles.footerIcon}>
-          <Link href={'https://piwoharnas.pl'}>
-            <a className={styles.link}><IconBrandFacebook size={30} stroke={1.1} color='black' /></a>
-          </Link>
-          <Link href={'https://piwoharnas.pl'}>
-            <a className={styles.link}><IconBrandInstagram size={30} stroke={1.1} color='black' /></a>
-          </Link>
-        </div>
-      </div> */
